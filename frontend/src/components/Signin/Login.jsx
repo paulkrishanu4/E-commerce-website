8e327746e94b7c60c02dbfcf9d654e1ee4950ea5
Krishanu Paul <paulkrishanu84@gmail.com>
2024-07-15 13:38:20 +0530

    Update Login.jsx

--- frontend/src/components/Signin/Login.jsx
+++ frontend/src/components/Signin/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
     if(!email || !password){
       alert("Fill all inputs");
     }else{
-    const response = await fetch("http://localhost:5000/login",{
+    const response = await fetch("https://e-commerce-website-backend-93ax.onrender.com/login",{
       method: "POST",
       headers:{
         "Content-Type": "application/json",
